b8836bc2bbf72807ed943fe64bf67647bf4159ea
Lindsey White <LindseyWhite@RocketMortgage.com>
2022-10-11 13:13:55 -0400

    initial commit

---
+++ XkcdConsole/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.Write("Enter a number to view a comic: ");
+
+int input = int.Parse(Console.ReadLine().Trim());
+
+// API: https://xkcd.com/info.0.json
+
+// Connects to the server 
+HttpClient web = new HttpClient();
+
+//separated into the base website and the path 
+//base website
+web.BaseAddress = new Uri("https://xkcd.com/");
+
+// set up a connection with a path 
+var connection = await web.GetAsync($"{input}/info.0.json");
+
+// Connect to the server and create a class with the data returned
+Comic com = await connection.Content.ReadAsAsync<Comic>();
+
+
+//// communicate with the server and return a string 
+//string result = await connection.Content.ReadAsStringAsync();
+
+//Console.WriteLine(result);
+
+
+
+Console.WriteLine(com.alt);
+Console.WriteLine(com.img);
+
+
+class Comic
+{
+    public int month { get; set; }
+    public int num { get; set; }
+    public string link { get; set; }
+    public int year { get; set; }
+    public string news { get; set; }
+    public string safe_title { get; set; }
+    public string transcript { get; set; }
+    public string alt { get; set; }
+    public string img { get; set; }
+    public string title { get; set; }
+    public int day { get; set; }
+}
+
---
+++ XkcdMvc/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using XkcdMvc.Models;
+
+
+namespace XkcdMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //Wrap return type within Task:  Task<IActionResult> 
+        //Add "async" before the word public
+        async public Task<IActionResult> Index()
+        {
+            HttpClient web = new HttpClient();
+            web.BaseAddress = new Uri("https://xkcd.com/");
+
+            //await can only be used within an async method 
+            var connection = await web.GetAsync("info.0.json");
+            Comic com = await connection.Content.ReadAsAsync<Comic>();
+            return View(com);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
